d257f557c50e351a09639ff04e6cfcbaf049f770
swapnilahmedshishir <ahmed66882298@gmail.com>
2025-02-13 22:36:46 +0600

    update the content marketing page

--- src/Component/Services/Marketing/ContentMarketing.jsx
+++ src/Component/Services/Marketing/ContentMarketing.jsx
@@ -45,14 +45,53 @@ const ContentMarketing = () => {
       <section className="uiuxDesign_hero_section">
         <div className="container webContentWriting_hero_part">
           <div className="webContentWriting_hero_text_part">
-            <h1>
-              Establish Your Business with TOJO Global's Content Marketing
-            </h1>
-            <p>
-              TOJO Global offers complete content marketing services designed to
-              help businesses create, distribute, and optimize content that
-              engages audiences and drives growth.
-            </p>
+            {isMobileView ? (
+              <>
+                {" "}
+                <div className="hero_page_strongText">
+                  <h2
+                    style={{
+                      ...(isMobileView && {
+                        paddingRight: "0rem",
+                      }),
+                    }}
+                  >
+                    Establish Your{" "}
+                    <span className="headdingGradientText"> Business </span>
+                  </h2>
+                  <h3 className="homePageHeroSubHeadingFristText">
+                    with TOJO Global's{" "}
+                  </h3>
+                  <h3 className="homePageHeroSubHeadingSecondText">
+                    Content Marketing
+                  </h3>
+                </div>
+                <p
+                  style={{
+                    ...(isMobileView && {
+                      textAlign: "justify",
+                    }),
+                  }}
+                >
+                  TOJO Global offers complete content marketing services
+                  designed to help businesses create, distribute, and optimize
+                  content that engages audiences and drives growth.
+                </p>
+              </>
+            ) : (
+              <>
+                {" "}
+                <h1>
+                  Establish Your Business with TOJO Global's Content Marketing
+                </h1>
+                <p>
+                  TOJO Global offers complete content marketing services
+                  designed to help businesses create, distribute, and optimize
+                  content that engages audiences and drives growth.
+                </p>
+              </>
+            )}
+
             <div className="hero_btn_style">
               <a href="https://www.behance.net/TOJO_Global" target="_blank">
                 <button className="custombtn hero_btn1 secondBtn">
@@ -100,8 +139,25 @@ const ContentMarketing = () => {
       {/* Our Content Marketing Services */}
       <section className="container">
         <div className="sectionTopTextstyle" data-aos="fade-up">
-          <h1 className="topHeadingStyle">Our Content Marketing Services</h1>
-          <p className="paraStyle">
+          <h1
+            className="topHeadingStyle"
+            style={{
+              ...(isMobileView && {
+                marginTop: "35px",
+              }),
+            }}
+          >
+            Our Content Marketing Services
+          </h1>
+          <p
+            className="paraStyle"
+            style={{
+              ...(isMobileView && {
+                textAlign: "justify",
+                fontSize: "14px",
+              }),
+            }}
+          >
             We provide a range of services that support your content marketing
             efforts at every stage:
           </p>
@@ -114,7 +170,14 @@ const ContentMarketing = () => {
             data-aos-delay={1 * 200}
           >
             <div className="our_contentMarketing_services_text_div">
-              <h2 className="elementHeadingText">
+              <h2
+                className="elementHeadingText"
+                style={{
+                  ...(isMobileView && {
+                    fontSize: "20px",
+                  }),
+                }}
+              >
                 Content Strategy Development{" "}
               </h2>
               <p>
@@ -132,7 +195,16 @@ const ContentMarketing = () => {
             data-aos-delay={2 * 200}
           >
             <div className="our_contentMarketing_services_text_div">
-              <h2 className="elementHeadingText">Content Creation </h2>
+              <h2
+                className="elementHeadingText"
+                style={{
+                  ...(isMobileView && {
+                    fontSize: "20px",
+                  }),
+                }}
+              >
+                Content Creation{" "}
+              </h2>
               <p>
                 Our team of professional writers, designers, and content
                 creators develop high-quality content tailored to your brand’s
@@ -150,7 +222,14 @@ const ContentMarketing = () => {
             data-aos-delay={3 * 200}
           >
             <div className="our_contentMarketing_services_text_div">
-              <h2 className="elementHeadingText">
+              <h2
+                className="elementHeadingText"
+                style={{
+                  ...(isMobileView && {
+                    fontSize: "20px",
+                  }),
+                }}
+              >
                 Search Engine Optimization (SEO)
               </h2>
               <p>
@@ -168,7 +247,16 @@ const ContentMarketing = () => {
             data-aos-delay={4 * 200}
           >
             <div className="our_contentMarketing_services_text_div">
-              <h2 className="elementHeadingText">Content Distribution</h2>
+              <h2
+                className="elementHeadingText"
+                style={{
+                  ...(isMobileView && {
+                    fontSize: "20px",
+                  }),
+                }}
+              >
+                Content Distribution
+              </h2>
               <p>
                 To ensure your content reaches the right people, we distribute
                 it across the most relevant channels, including social media,
@@ -185,7 +273,14 @@ const ContentMarketing = () => {
             data-aos-delay={5 * 200}
           >
             <div className="our_contentMarketing_services_text_div">
-              <h2 className="elementHeadingText">
+              <h2
+                className="elementHeadingText"
+                style={{
+                  ...(isMobileView && {
+                    fontSize: "20px",
+                  }),
+                }}
+              >
                 Performance Tracking and Reporting
               </h2>
               <p>
@@ -226,7 +321,16 @@ const ContentMarketing = () => {
               <h4 className="appDev_industries_services_subHeading">
                 Effective Solutions
               </h4>
-              <p className="contentMarketingService_paraTextStyle">
+              <p
+                className="contentMarketingService_paraTextStyle"
+                style={{
+                  ...(isMobileView && {
+                    textAlign: "justify",
+                    paddingLeft: "5px",
+                    paddingRight: "5px",
+                  }),
+                }}
+              >
                 TOJO Global helps businesses create, distribute, and optimize
                 content that engages audiences and drives growth, increasing
                 online visibility and attracting more customers.
@@ -251,7 +355,16 @@ const ContentMarketing = () => {
               <h4 className="appDev_industries_services_subHeading">
                 Experienced Team
               </h4>
-              <p className="contentMarketingService_paraTextStyle">
+              <p
+                className="contentMarketingService_paraTextStyle"
+                style={{
+                  ...(isMobileView && {
+                    textAlign: "justify",
+                    paddingLeft: "5px",
+                    paddingRight: "5px",
+                  }),
+                }}
+              >
                 Our skilled team of content creators, SEO specialists, and
                 marketers ensures your content is high-quality, relevant, and
                 competitive, keeping your brand ahead of the market trends.
@@ -276,7 +389,16 @@ const ContentMarketing = () => {
               <h4 className="appDev_industries_services_subHeading">
                 Data-Driven Approach
               </h4>
-              <p className="contentMarketingService_paraTextStyle">
+              <p
+                className="contentMarketingService_paraTextStyle"
+                style={{
+                  ...(isMobileView && {
+                    textAlign: "justify",
+                    paddingLeft: "5px",
+                    paddingRight: "5px",
+                  }),
+                }}
+              >
                 We base our content marketing strategies on data and analytics,
                 ensuring every decision is backed by solid insights. This
                 enables us to refine and improve strategies over time for
@@ -302,7 +424,16 @@ const ContentMarketing = () => {
               <h4 className="appDev_industries_services_subHeading">
                 Proven Results
               </h4>
-              <p className="contentMarketingService_paraTextStyle">
+              <p
+                className="contentMarketingService_paraTextStyle"
+                style={{
+                  ...(isMobileView && {
+                    textAlign: "justify",
+                    paddingLeft: "5px",
+                    paddingRight: "5px",
+                  }),
+                }}
+              >
                 Our content marketing services have driven measurable
                 improvements in website traffic, search rankings, and lead
                 generation for our clients across different industries.
@@ -327,7 +458,16 @@ const ContentMarketing = () => {
               <h4 className="appDev_industries_services_subHeading">
                 Comprehensive Services
               </h4>
-              <p className="contentMarketingService_paraTextStyle">
+              <p
+                className="contentMarketingService_paraTextStyle"
+                style={{
+                  ...(isMobileView && {
+                    textAlign: "justify",
+                    paddingLeft: "5px",
+                    paddingRight: "5px",
+                  }),
+                }}
+              >
                 We handle everything from planning, content creation, and
                 distribution to optimization, providing you with a complete
                 content marketing service that drives business growth.
@@ -352,7 +492,16 @@ const ContentMarketing = () => {
               <h4 className="appDev_industries_services_subHeading">
                 Customizable Strategies
               </h4>
-              <p className="contentMarketingService_paraTextStyle">
+              <p
+                className="contentMarketingService_paraTextStyle"
+                style={{
+                  ...(isMobileView && {
+                    textAlign: "justify",
+                    paddingLeft: "5px",
+                    paddingRight: "5px",
+                  }),
+                }}
+              >
                 We create tailored content marketing strategies that suit your
                 business needs. Our flexible approach ensures your unique goals
                 are met, helping you grow and stand out in your industry.
@@ -380,10 +529,27 @@ const ContentMarketing = () => {
               <h1>01</h1>
             </div>
             <div className="contentMarketing_services_text_div">
-              <h4 className="appDev_industries_services_subHeading">
+              <h4
+                className="appDev_industries_services_subHeading"
+                style={{
+                  ...(isMobileView && {
+                    paddingLeft: "6px",
+                  }),
+                }}
+              >
                 Increased Brand Awareness
               </h4>
-              <p className="contentMarketingService_paraTextStyle">
+              <p
+                className="contentMarketingService_paraTextStyle"
+                style={{
+                  ...(isMobileView && {
+                    textAlign: "justify",
+                    fontSize: "12px",
+                    paddingLeft: "5px",
+                    paddingRight: "5px",
+                  }),
+                }}
+              >
                 By consistently producing valuable content, you make your brand
                 more visible to your target audience. This will help you to grow
                 your brand’s recognition and trust.
@@ -403,7 +569,15 @@ const ContentMarketing = () => {
               <h4 className="appDev_industries_services_subHeading">
                 Better Search Engine Rankings
               </h4>
-              <p className="contentMarketingService_paraTextStyle">
+              <p
+                className="contentMarketingService_paraTextStyle"
+                style={{
+                  ...(isMobileView && {
+                    textAlign: "justify",
+                    fontSize: "12px",
+                  }),
+                }}
+              >
                 Our SEO-optimized content improves your website’s ranking on
                 search engines like Google, making it easier for potential
                 customers to find you.
@@ -423,7 +597,15 @@ const ContentMarketing = () => {
               <h4 className="appDev_industries_services_subHeading">
                 Enhanced Customer Engagement
               </h4>
-              <p className="contentMarketingService_paraTextStyle">
+              <p
+                className="contentMarketingService_paraTextStyle"
+                style={{
+                  ...(isMobileView && {
+                    textAlign: "justify",
+                    fontSize: "12px",
+                  }),
+                }}
+              >
                 Engaging content encourages your audience to interact with your
                 brand, fostering stronger relationships and building customer
                 loyalty.
@@ -443,7 +625,15 @@ const ContentMarketing = () => {
               <h4 className="appDev_industries_services_subHeading">
                 Lead Generation
               </h4>
-              <p className="contentMarketingService_paraTextStyle">
+              <p
+                className="contentMarketingService_paraTextStyle"
+                style={{
+                  ...(isMobileView && {
+                    textAlign: "justify",
+                    fontSize: "12px",
+                  }),
+                }}
+              >
                 Content marketing attracts qualified leads who are genuinely
                 interested in your products or services. This will increase the
                 chances of converting them into customers.
@@ -463,7 +653,15 @@ const ContentMarketing = () => {
               <h4 className="appDev_industries_services_subHeading">
                 Cost-Effective Marketing
               </h4>
-              <p className="contentMarketingService_paraTextStyle">
+              <p
+                className="contentMarketingService_paraTextStyle"
+                style={{
+                  ...(isMobileView && {
+                    textAlign: "justify",
+                    fontSize: "12px",
+                  }),
+                }}
+              >
                 Compared to traditional advertising, content marketing is more
                 affordable and sustainable. Once developed, content keeps
                 attracting traffic and generating leads over time, offering
@@ -484,7 +682,15 @@ const ContentMarketing = () => {
               <h4 className="appDev_industries_services_subHeading">
                 Long-Term Impact
               </h4>
-              <p className="contentMarketingService_paraTextStyle">
+              <p
+                className="contentMarketingService_paraTextStyle"
+                style={{
+                  ...(isMobileView && {
+                    textAlign: "justify",
+                    fontSize: "12px",
+                  }),
+                }}
+              >
                 Content remains online long after it’s published, continuing to
                 attract visitors and generate leads well beyond its initial
                 release, unlike paid ads that only work while the budget is
@@ -496,7 +702,15 @@ const ContentMarketing = () => {
       </section>
 
       {/*Ready to Elevate Your White Papers?*/}
-      <section className="container">
+      <section
+        className="container"
+        style={{
+          ...(isMobileView && {
+            marginTop: "3rem",
+            marginBottom: "2.5rem",
+          }),
+        }}
+      >
         <div
           className="professionalModeration_div blockChainSolution_specialize_card"
           data-aos="fade-down"
@@ -505,7 +719,17 @@ const ContentMarketing = () => {
             <h1 className="professionalModeration_service_offers_heading">
               Ready to Elevate Your White Papers?
             </h1>
-            <p className="professionalModeration_service_offers_para">
+            <p
+              className="professionalModeration_service_offers_para"
+              style={{
+                ...(isMobileView && {
+                  textAlign: "justify",
+                  fontSize: "12px",
+                  paddingLeft: "5px",
+                  paddingRight: "5px",
+                }),
+              }}
+            >
               {" "}
               A well-designed white paper can be a powerful tool for your
               business, helping you communicate complex ideas while reinforcing
